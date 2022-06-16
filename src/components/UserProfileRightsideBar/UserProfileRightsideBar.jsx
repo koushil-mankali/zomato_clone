7be71f95277c4807752b2a17a85fb4b8c5a0897b
koushil-mankali <koushilmankali@gmail.com>
2022-06-16 11:14:20 +0530

    UserReviewedCard Done

--- src/components/UserProfileRightsideBar/UserProfileRightsideBar.jsx
+++ src/components/UserProfileRightsideBar/UserProfileRightsideBar.jsx
@@ -2,11 +2,104 @@ import {useParams } from 'react-router-dom'
 
 import css from './UserProfileRightsideBar.module.css'
 
+import ImgSrc from '../../../public/images/proandproplus.jpg';
+
 import UserReviewedCard from '../../utils/UserReviewedCard/UserReviewedCard'
 
 let UserProfileRightsideBar = () => {
 
     let {userId, hashId} = useParams(); 
+
+    let data = [{
+        imgSrc: ImgSrc,
+        title: "Paradise Biryani",
+        address: "Kukatpally, Hyd",
+        stars: 5,
+        days: 10,
+        votes: 10,
+        comments: 2,
+        id: 123,
+        userImg: ImgSrc,
+        userId: userId,
+    },
+    {
+        imgSrc: ImgSrc,
+        title: "Paradise Biryani",
+        address: "Kukatpally, Hyd",
+        stars: 5,
+        days: 10,
+        votes: 10,
+        comments: 2,
+        id: 123,
+        userImg: ImgSrc,
+        userId: userId,
+    },{
+        imgSrc: ImgSrc,
+        title: "Paradise Biryani",
+        address: "Kukatpally, Hyd",
+        stars: 5,
+        days: 10,
+        votes: 10,
+        comments: 2,
+        id: 123,
+        userImg: ImgSrc,
+        userId: userId,
+    },{
+        imgSrc: ImgSrc,
+        title: "Paradise Biryani",
+        address: "Kukatpally, Hyd",
+        stars: 5,
+        days: 10,
+        votes: 10,
+        comments: 2,
+        id: 123,
+        userImg: ImgSrc,
+        userId: userId,
+    },{
+        imgSrc: ImgSrc,
+        title: "Paradise Biryani",
+        address: "Kukatpally, Hyd",
+        stars: 5,
+        days: 10,
+        votes: 10,
+        comments: 2,
+        id: 123,
+        userImg: ImgSrc,
+        userId: userId,
+    },{
+        imgSrc: ImgSrc,
+        title: "Paradise Biryani",
+        address: "Kukatpally, Hyd",
+        stars: 5,
+        days: 10,
+        votes: 10,
+        comments: 2,
+        id: 123,
+        userImg: ImgSrc,
+        userId: userId,
+    },{
+        imgSrc: ImgSrc,
+        title: "Paradise Biryani",
+        address: "Kukatpally, Hyd",
+        stars: 5,
+        days: 10,
+        votes: 10,
+        comments: 2,
+        id: 123,
+        userImg: ImgSrc,
+        userId: userId,
+    },{
+        imgSrc: ImgSrc,
+        title: "Paradise Biryani",
+        address: "Kukatpally, Hyd",
+        stars: 5,
+        days: 10,
+        votes: 10,
+        comments: 2,
+        id: 123,
+        userImg: ImgSrc,
+        userId: userId,
+    }]
 
     console.log(userId, hashId, "user and hash id");
 
@@ -14,7 +107,9 @@ let UserProfileRightsideBar = () => {
         <div className={css.innerDiv}>
             <div className={css.title}>Reviews</div>
             <div className={css.contectBox}>
-                    <UserReviewedCard />
+                {data?.map(val => {
+                    return <UserReviewedCard data={val} />
+                })}
             </div>
         </div>
     </div>
