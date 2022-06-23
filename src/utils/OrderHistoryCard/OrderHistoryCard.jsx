d9660ae73c3f0b442ece8624b90f6bd58f6777b5
koushil-mankali <koushilmankali@gmail.com>
2022-06-23 12:19:31 +0530

    OrderHistoryCard structure setup

--- src/utils/OrderHistoryCard/OrderHistoryCard.jsx
+++ src/utils/OrderHistoryCard/OrderHistoryCard.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import css from './OrderHistoryCard.module.css'
+import React from "react";
 
-const OrderHistoryCard = () => {
+import css from "./OrderHistoryCard.module.css";
+
+const OrderHistoryCard = ({ data }) => {
+  const {
+    id,
+    imgSrc,
+    name,
+    address,
+    orderNum,
+    items,
+    orderedOn,
+    itemTotal,
+    coupon,
+    taxesandcharges,
+    totalSavings,
+    grandTotal,
+    paymentType,
+    phoneNum,
+    deliveredTo,
+    fssaiNo,
+    fav,
+    summaryLinkId,
+  } = data;
   return (
-    <div>OrderHistoryCard</div>
-  )
-}
+    <div className={css.outerDiv}>
+      <div className={css.innerDiv}>
+        
+      </div>
+    </div>
+  );
+};
 
-export default OrderHistoryCard
+export default OrderHistoryCard;
