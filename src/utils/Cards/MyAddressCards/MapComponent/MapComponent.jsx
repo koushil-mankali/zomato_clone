c71cb721977d9913319005571f74c28b893babbd
koushil-mankali <koushilmankali@gmail.com>
2022-07-04 10:00:12 +0530

    Removed google maps package and code

--- src/utils/Cards/MyAddressCards/MapComponent/MapComponent.jsx
+++ src/utils/Cards/MyAddressCards/MapComponent/MapComponent.jsx
@@ -1,38 +1,9 @@
-import {Component} from "react";
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React from 'react'
 
-import css from './MapComponent.module.css'
- 
-export class MapComponent extends Component {
-    constructor(){
-        super()
-        this.state = {
-            selectedPlace: {
-                name: "Hyderabad"
-            },
-            lat: navigator.geolocation.getCurrentPosition(val => val.coords.latitude),
-            lng: navigator.geolocation.getCurrentPosition(val => val.coords.longitude)
-        }
-    }
-   
-  render() {
-    console.log(navigator.geolocation.getCurrentPosition(val => {return val}), "test")
-    return (
-      <Map google={this.props.google} zoom={14} style={{width: "600px", height: "50%"}}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-      </Map>
-    );
-  }
+const MapComponent = () => {
+  return (
+    <div>MapComponent</div>
+  )
 }
- 
-export default GoogleApiWrapper({
-  apiKey: ("AIzaSyBhKe3SSGLFTzZc6L4XXCsBBhvq2R5Ohmg")
-})(MapComponent)
+
+export default MapComponent
