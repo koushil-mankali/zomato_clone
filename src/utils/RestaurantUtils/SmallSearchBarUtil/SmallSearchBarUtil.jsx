184c816681f356b044cb4a9814cf9490138c0bf1
koushil-mankali <koushilmankali@gmail.com>
2022-07-08 17:40:25 +0530

    SmallSearchBarUtil WIP

--- src/utils/RestaurantUtils/SmallSearchBarUtil/SmallSearchBarUtil.jsx
+++ src/utils/RestaurantUtils/SmallSearchBarUtil/SmallSearchBarUtil.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import css from './SmallSearchBarUtil.module.css'
+
 const SmallSearchBarUtil = () => {
-  return (
-    <div>SmallSearchBarUtil</div>
-  )
+  return <div className={css.outerDiv}>
+    <div className={css.innerDiv}>
+      <div className={css.searchBox}>
+        <img src="" alt="cancel icon" />
+        <input type="search" placeholder="Search within menu" />
+        <img src="" alt="search icon" />
+      </div>
+    </div>
+  </div>
 }
 
 export default SmallSearchBarUtil
